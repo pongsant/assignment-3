b9cf64dc88bca17beb2cb714d26d3df2141e5d6f
pongsant <1pongsant@gmail.com>
2025-11-03 17:52:15 -0500

    n

--- source/main.jsx
+++ source/main.jsx
@@ -1,11 +1,12 @@
+// source/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Song from "./pages/Song.jsx";
 import "./styles.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <Home /> },
   { path: "/song/:id", element: <Song /> }
 ]);
